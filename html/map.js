2a12b918123d27944d995f6d0534fa52d22bb436
Denn Guerrier <thenarius@gmail.com>
2016-05-21 07:46:35 -0700

    updated map page with foundations for functionality

--- html/map.js
+++ html/map.js
@@ -1,15 +1,19 @@
 var app = angular.module('app', []);
 
-app.controller("main", function($scope) {
+app.controller("main", ['$scope', '$http', function($scope, $http) {
 	$scope.test = "test";
-})
+	$scope.loadData = function() {
+		console.log("init");
+		$http.get('/dioceses/olympia.json').then(function(data) { 
+			$scope.parishes = data.data.parishes;
+		});
+	}
+}]);
 
-// <script>
-// 	var map;
-// 	function initMap() {
-// 		map = new google.maps.Map(document.getElementById('map'), {
-// 			center: {lat: -34.397, lng: 150.644},
-// 			zoom: 8
-// 		});
-// 	}
-// </script>
+// var map;
+// function initMap() {
+// 	map = new google.maps.Map(document.getElementById('map'), {
+// 		center: {lat: -34.397, lng: 150.644},
+// 		zoom: 8
+// 	});
+// }
